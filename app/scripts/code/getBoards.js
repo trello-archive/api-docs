df3d4d99eae07deef50fc4c6312ef53172f30daa
Stephen Fluin <stephen@trello.com>
2015-06-02 13:01:21 -0500

    Added additional content, updated code labs. Added more Trello Styling

--- app/scripts/code/getBoards.js
+++ app/scripts/code/getBoards.js
@@ -9,6 +9,3 @@ var error = function(errorMsg) {
 };
 
 Trello.get('/member/me/boards', success, error);
-
-//Trello.post('/webhooks', {description: "do cool stuff", callbackURL: 'http://mortalpowers.com/hook.php', idModel:'555c8e8438613a1b6f665efc'}, success, error);
-//Trello.get('/tokens/6ce9e4bf456928dd731f1e2d2ce8d94ed007aaf54924720ec2fe42f2d7d19bf1/webhooks',success,error);
