25c67a2eb940c4c5e20c63391dee3672bb88d9e5
Stephen Fluin <stephen@trello.com>
2015-06-24 20:09:37 -0500

    Updated code to fit new style and use asyncoutput

--- app/scripts/code/createCard.js
+++ app/scripts/code/createCard.js
@@ -2,19 +2,19 @@
 var destinationList = "";
 
 var success = function(successMsg) {
-  output(successMsg);
+  asyncOutput(successMsg);
 };
 
 var error = function(errorMsg) {
-  output("error: " + errorMsg);
+  asyncOutput("error: " + errorMsg);
 };
 
 var newCard = 
-    		{name: "I just created a new card!", 
-    		desc: "Using the Trello API is fun and easy!",
-    		pos: "top", 
-    		due: null,
-			idList: destinationList
-    		};
+  {name: "I just created a new card!", 
+  desc: "Using the Trello API is fun and easy!",
+  pos: "top", 
+  due: null,
+  idList: destinationList
+  };
 
 Trello.post('/cards/', newCard, success, error);
--- app/scripts/code/createWebhook.js
+++ app/scripts/code/createWebhook.js
@@ -9,9 +9,9 @@ var error = function(errorMsg) {
 };
 
 var parameters = {
-	description: "A sample webhook created from the sandbox on a list",
-	callbackURL: "http://example.com/YOURURL",
-	idModel: "555c8e8438613a1b6f665efc"
+  description: "A sample webhook created from the sandbox on a list",
+  callbackURL: "http://example.com/YOURURL",
+  idModel: "555c8e8438613a1b6f665efc"
 };
 
 Trello.post('/webhooks/', parameters, success, error);
