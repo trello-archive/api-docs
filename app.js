c1e5b4d5efeee4a00055c72257af3d3a9a5cca8a
Stephen Fluin <stephen@trello.com>
2015-09-09 07:53:02 -0500

    Fixed port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.all("/*", function(req, res, next) {
 });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
