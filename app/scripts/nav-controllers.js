95a7d86b5cc55e300f58dbf6b1c1a4cf41a184fe
Stephen Fluin <stephen@trello.com>
2016-01-14 15:49:37 -0600

    Added nav active link styling

---
+++ app/scripts/nav-controllers.js
@@ -0,0 +1,10 @@
+var app = angular.module('BuildWithTrelloControllers');
+
+app.controller('NavCtrl', function($scope, $state) {
+	$scope.navClass = function(page) {
+		if($state.is(page)) {
+			return "active";
+		}
+
+	};
+});
