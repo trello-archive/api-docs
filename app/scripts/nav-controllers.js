af985a65630274c35038037e6e527d83cef5ab77
Stephen Fluin <stephen@trello.com>
2016-01-14 15:53:12 -0600

    Fixed name of active class per CSS standards

--- app/scripts/nav-controllers.js
+++ app/scripts/nav-controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('BuildWithTrelloControllers');
 app.controller('NavCtrl', function($scope, $state) {
 	$scope.navClass = function(page) {
 		if($state.is(page)) {
-			return "active";
+			return "is-active";
 		}
 
 	};
