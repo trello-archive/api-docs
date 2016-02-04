56112417b6f2fd57f8754381684f54c9e5e8f80c
Stephen Fluin <stephen@trello.com>
2016-02-04 17:11:07 -0600

    Trying to remove opbeat errors

--- app/scripts/nav-controllers.js
+++ app/scripts/nav-controllers.js
@@ -2,7 +2,10 @@ var app = angular.module('BuildWithTrelloControllers');
 
 app.controller('NavCtrl', function($scope, $state) {
 	$scope.navClass = function(page) {
-		if($state.current.name.startsWith(page)) {
+		if($state.current &&
+			$state.current.name &&
+			$state.current.name.startsWith &&
+			$state.current.name.startsWith(page)) {
 			return "is-active";
 		}
 
